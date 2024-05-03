031ad6485c9f461f656ba00883a98a69968f1880
Evelina Kalinnikova <kalinnikova770@gmail.com>
2024-05-03 15:21:23 +0300

    third task with geolocation

--- task 3/js-code.js
+++ task 3/js-code.js
@@ -4,6 +4,9 @@ const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
 const chat = document.querySelector('.chat');
 
+const geoBtn = document.querySelector('.geo-btn');
+const mapLink = document.querySelector('.map-link');
+
 
 function writeToScreen(message) {
     const p = document.createElement('p');
@@ -31,3 +34,24 @@ btn.addEventListener('click', ()=> {
       '<span style="color: red;">ERROR:</span> ' + evt.data
     );
   };
+
+geoBtn.addEventListener('click', ()=> {
+    if (!navigator.geolocation) {
+        alert('Геолокация не поддерживается вашим браузером');
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+})
+
+const success = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude,longitude);
+    mapLink.setAttribute('href', `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`);
+    mapLink.textContent = 'Открыть карту';
+    mapLink.style.display = 'block';
+}
+
+const error = () => {
+    alert('Не удалось получить геолокацию');
+}
